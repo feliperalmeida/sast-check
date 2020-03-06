8a26cc8bfab9d9d43e8f5be2d3d7f35eac3bc202
Felipe Rosa de Almeida <felipe.almeida@loadsmart.com>
2020-03-06 18:52:37 -0300

    Proper exit code

--- docker/sast-check.sh
+++ docker/sast-check.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-RESULT=$(bandit --version && bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports "$@")
+RESULT=$(bandit --version && bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports,venv "$@")
+EXITCODE=$?
 
 RESULT="${RESULT//'%'/'%25'}"
 RESULT="${RESULT//$'\n'/'%0A'}"
 RESULT="${RESULT//$'\r'/'%0D'}"
 echo "::set-output name=result::${RESULT}"
+
+exit ${EXITCODE}
