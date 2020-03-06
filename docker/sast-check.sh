cb96b275e17b769eb61aadd08fbc0fe09cffedae
Felipe Rosa de Almeida <felipe.almeida@loadsmart.com>
2020-03-06 17:16:16 -0300

    Make output compatible with GitHub Action format

--- docker/sast-check.sh
+++ docker/sast-check.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-set -eax
-bandit --version
-bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports "$@"
+RESULT=$(bandit --version && bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports "$@")
+
+RESULT="${RESULT//'%'/'%25'}"
+RESULT="${RESULT//$'\n'/'%0A'}"
+RESULT="${RESULT//$'\r'/'%0D'}"
+echo "::set-output name=result::${RESULT}"
