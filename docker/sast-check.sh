a9c1da19490483e2455d655eb9d3dae81abfb417
Felipe Rosa de Almeida <felipe.almeida@loadsmart.com>
2020-03-06 19:00:49 -0300

    Remove GH custom output

--- docker/sast-check.sh
+++ docker/sast-check.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-RESULT=$(bandit --version && bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports,venv "$@")
-EXITCODE=$?
+set -euo pipefail
+bandit --version
+bandit -r -a vuln -ii -ll -x .git,.svn,.mvn,.idea,dist,bin,obj,backup,docs,tests,test,tmp,reports,venv "$@"
+# EXITCODE=$?
 
-RESULT="${RESULT//'%'/'%25'}"
-RESULT="${RESULT//$'\n'/'%0A'}"
-RESULT="${RESULT//$'\r'/'%0D'}"
-echo "::set-output name=result::${RESULT}"
+# RESULT="${RESULT//'%'/'%25'}"
+# RESULT="${RESULT//$'\n'/'%0A'}"
+# RESULT="${RESULT//$'\r'/'%0D'}"
+# echo "::set-output name=result::${RESULT}"
 
-exit ${EXITCODE}
+# exit ${EXITCODE}
